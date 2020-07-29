0727712793f714a410e377d99288aef7ee128399
barackmaund1 <pmaundu49@gmail.co>
2020-07-29 12:12:49 +0300

    Updates

---
+++ precache-manifest.d124da2baeb96b4b95f1f4b832aa043a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73da4e44e6dae2f8aa0aed021b8551f5",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "014f9de2cb9c58a282e2",
+    "url": "/react-app/static/css/2.e962c2eb.chunk.css"
+  },
+  {
+    "revision": "834afcce4997c0a29d3a",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "014f9de2cb9c58a282e2",
+    "url": "/react-app/static/js/2.355348e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.355348e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "834afcce4997c0a29d3a",
+    "url": "/react-app/static/js/main.57812e1c.chunk.js"
+  },
+  {
+    "revision": "113281ebc675ba78a95d",
+    "url": "/react-app/static/js/runtime-main.b17fe779.js"
+  }
+]);
